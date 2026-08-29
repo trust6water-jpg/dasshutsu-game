a8162e13caf539c4f6f12c0190dbbfa5b18bf906
お湯 <trust6water@gmail.com>
2026-08-29 16:17:31 +0900

    Update server.js

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ async function handleEvent(event) {
         }
         return await client.replyMessage(event.replyToken, {
           type: 'text',
-          text: '【園内共通のマスターキー】を手に入れた！\n※マスターキーは園内の建物なら、なんでも開閉できるよ\n（「持ち物」と入力すれば、使用する画面が出てくるよ）'
+          text: '【園内共通のマスターキー】を手に入れた！\n※マスターキーは動物園入り口以外の園内の建物なら、なんでも開閉できるよ\n（「持ち物」と入力すれば、使用する画面が出てくるよ）'
         });
       }
 
